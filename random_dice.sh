e5bf0959c0516b219e9750b4307c70c0f3dcf72d
tusharranand <tusharranand@gmail.com>
2022-02-12 15:39:14 +0530

    Sequence Practice Problems 1,2 and 3

---
+++ random_dice.sh
@@ -0,0 +1,24 @@
+
+# 1. Use Random Function ((RANDOM)) to get Single Digit
+
+single=$((RANDOM%10))
+
+echo "A random single number digit is: " $single
+echo ""
+
+# 2. Use Random to get Dice Number between 1 to 6
+
+dice=$((RANDOM%6+1))
+
+echo "A random dice roll gives: " $dice
+echo ""
+
+# 3. Add two Random Dice Number and Print the Result
+
+dice1=$((RANDOM%6+1))
+dice2=$((RANDOM%6+1))
+sum=$(($dice1+$dice2))
+
+echo "First random dice roll gives: " $dice1
+echo "Second random dice roll gives: " $dice2
+echo "Sum of the two dice rolls is: " $sum
